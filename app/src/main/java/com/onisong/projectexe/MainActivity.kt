79ba8012d634c32677bafa7860c23f706255a8ab
OniSong <Onisongproject.exe@gmail.com>
2026-03-15 18:53:16 -0700

    Add System Alert Window permission and request logic

--- app/src/main/java/com/onisong/projectexe/MainActivity.kt
+++ app/src/main/java/com/onisong/projectexe/MainActivity.kt
@@ -1,9 +1,27 @@
 package com.onisong.projectexe
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkOverlayPermission()
+    }
+
+    private fun checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, 1234)
+            }
+        }
     }
 }
